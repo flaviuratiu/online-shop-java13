ecb4b85b8f88c691fedf2d098adefa7b9a2a5528
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-12-02 19:31:00 +0200

    added getProducts method

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -3,11 +3,14 @@ package org.fasttrackit.onlineshop.service;
 import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.ProductRepository;
+import org.fasttrackit.onlineshop.transfer.GetProductsRequest;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -53,6 +56,20 @@ public class ProductService {
                 // lambda expression
                 .orElseThrow(() -> new ResourceNotFoundException(
                         "Product " + id + " does not exist."));
+    }
+
+    public Page<Product> getProducts(GetProductsRequest request, Pageable pageable) {
+        LOGGER.info("Retrieving products: {}", request);
+        if (request != null && request.getPartialName() != null &&
+                request.getMinQuantity() != null) {
+            return productRepository.findByNameContainingAndQuantityGreaterThanEqual(
+                    request.getPartialName(), request.getMinQuantity(), pageable);
+        } else if (request != null && request.getPartialName() != null) {
+            return productRepository.findByNameContaining(
+                    request.getPartialName(), pageable);
+        } else {
+            return productRepository.findAll(pageable);
+        }
     }
 
     public Product updateProduct(long id, SaveProductRequest request) {
