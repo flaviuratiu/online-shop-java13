a3eb75965c0624c194f7150eabb4ee20d872896d
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-12-09 20:59:50 +0200

    added test for adding product to cart (work in progress)

--- src/test/java/org/fasttrackit/onlineshop/CartServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshop/CartServiceIntegrationTests.java
@@ -1,6 +1,9 @@
 package org.fasttrackit.onlineshop;
 
+import org.fasttrackit.onlineshop.domain.Customer;
 import org.fasttrackit.onlineshop.service.CartService;
+import org.fasttrackit.onlineshop.steps.CustomerSteps;
+import org.fasttrackit.onlineshop.transfer.AddProductToCartRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +17,16 @@ public class CartServiceIntegrationTests {
     @Autowired
     private CartService cartService;
 
+    @Autowired
+    private CustomerSteps customerSteps;
+
     @Test
     public void testAddProductToCart_whenNewCartForExistingCustomer_thenCartIsSaved() {
+        Customer customer = customerSteps.createCustomer();
 
+        AddProductToCartRequest request = new AddProductToCartRequest();
+        request.setCustomerId(customer.getId());
+
+        cartService.addProductToCart(request);
     }
 }
